f9be59dd83276557ac1df6ae9288daedc10a0fa9
Justin Ellingwood <justin.ellingwood@gmail.com>
2024-07-24 12:39:14 +0100

    update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export CUDA_HOME=/usr/local/cuda
-export PATH=${CUDA_HOME}/bin:$PATH
-export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:$LD_LIBRARY_PATH
+export PATH=${CUDA_HOME}/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=${CUDA_HOME}/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 
 jupyter lab --allow-root --no-browser --port=$PORT --ip=* --FileContentsManager.delete_to_trash=False --ServerApp.terminado_settings='{"shell_command":["/bin/bash"]}' --ServerApp.token=$JUPYTER_PASSWORD --ServerApp.allow_origin=* --ServerApp.preferred_dir=/workspace
